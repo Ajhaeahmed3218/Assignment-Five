9fa0b98d12a75d0a48124046a00a7ebefffee801
Ajharahmed3218 <nuhash3218@gmail.com>
2024-02-18 19:38:43 +0600

    Almost function Added

--- script.js
+++ script.js
@@ -40,9 +40,37 @@ let sum = 0;
     }
 
 
-// <tr>
-                              
-//                               <td>C2</td>
-//                               <td>Economoy</td>
-//                               <td>550</td>
-// </tr>
+   
+    
+
+
+    document.getElementById("apply-btn").addEventListener("click", function () {
+        let inputValue =document.getElementById("input-field").value
+        if (sum>=4) {
+            if (inputValue === "NEW15" && sum) {
+                let grandPrice=stringInNumber("grend-total")
+                let off=(grandPrice*15)/100
+                document.getElementById("grend-total").innerText=grandPrice-off
+                // let off =document.getElementById("discountPrice").innerText=(sumOfPrice*20)/100
+                // document.getElementById("total").innerText=sumOfPrice-off.toFixed(2)
+                hiddenElementById("lebel-input")
+    
+    
+            }else if(inputValue === "Couple 20"){
+                let grandPrice=stringInNumber("grend-total")
+                let off=(grandPrice*20)/100
+                document.getElementById("grend-total").innerText=grandPrice-off
+                hiddenElementById("lebel-input")
+
+            }else{
+                alert("Invalid Cupon")
+            }
+        }else{
+            alert("Bay More For Discount ")
+        }
+                 
+     })
+
+     if (sum>0) {
+        
+     }
--- utility.js
+++ utility.js
@@ -13,6 +13,16 @@ function  stringInNumber(idName) {
     return inNumber
 }
 
+function hiddenElementById(elementId) {
+    const element=document.getElementById(elementId)
+    element.classList.add("hidden")
+  
+}
+function showElementById(elementId) {
+    const element=document.getElementById(elementId)
+    element.classList.remove("hidden")
+  
+}
 
 
 
